4c0714cd2baecbe535eae59c3703cb1b07274608
José Manuel <josemanuel3606@gmail.com>
2024-12-28 21:20:41 +0100

    Testing scroll 1

--- js/products.js
+++ js/products.js
@@ -1,42 +1,83 @@
-import { getAPI } from "../utils/getAPI.js"
+import { getAPI } from "../utils/getAPI.js";
 
 
-const main = async () => {
-    const shopJSON = await getAPI('https://fortnite-api.com/v2/shop')
-    const products = document.querySelector('.products')
-    shopJSON.data.entries.forEach(item => {
+let currentIndex = 0;
+const limit = 24;
+let allDataLoaded = false; 
+
+const getData = async () => {
+    if (allDataLoaded) return;  
+
+    const shopJSON = await getAPI('https://fortnite-api.com/v2/shop');
+    const products = document.querySelector('.products');
+    
+    const shopWithLimit = shopJSON.data.entries.slice(currentIndex, currentIndex + limit);
+
+    if (shopWithLimit.length === 0) {
+        allDataLoaded = true;
+        observer.disconnect();
+        return;
+    }
+
+    shopWithLimit.forEach(item => {
         if ("brItems" in item) {
-            console.log(item);
-            const price = document.createElement('h4')
-            const product = document.createElement('article')
-            product.classList.add('product')
-            const name = document.createElement('h3')
-            const img = document.createElement('img')
+            const price = document.createElement('h4');
+            const product = document.createElement('article');
+            product.classList.add('product');
+            const name = document.createElement('h3');
+            const img = document.createElement('img');
 
-            price.textContent = item.regularPrice + ' V-Bucks'
+            price.textContent = item.regularPrice + ' V-Bucks';
 
             if ("bundle" in item) {
-                img.src = item.bundle.image
-                name.textContent = item.bundle.name
+                img.src = item.bundle.image;
+                name.textContent = item.bundle.name;
             } else {
                 item.brItems.forEach((item) => {
-                    if("images" in item) {
-                        if("bean" in item.images) {
-                            img.src = item.images.featured || item.images.icon
-                        } else {
-                            img.src = item.images.smallIcon
-                        }
+                    if ("images" in item) {
+                        img.src = item.images.featured || item.images.icon;
                     }
-                    name.textContent = item.name
-                })
+                    name.textContent = item.name;
+                });
             }
 
-            product.appendChild(img)
-            product.appendChild(name)
-            product.appendChild(price)
-            products.appendChild(product)
+            product.appendChild(img);
+            product.appendChild(name);
+            product.appendChild(price);
+            products.appendChild(product);
         }
     });
+
+    currentIndex += limit;
+};
+
+let observer;
+
+function createObserver() {
+    const products = document.querySelector('.products');
+    const observerDOM = document.createElement('div')
+    products.appendChild(observerDOM)
+    const boxElement = document.querySelector('#observer')
+
+    const options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0,  
+    };
+
+    observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(boxElement);
 }
 
-document.addEventListener("DOMContentLoaded", main)
+const handleIntersect = (entries) => {
+    if (entries[0].isIntersecting && !allDataLoaded) {
+        getData();
+    }
+};
+
+const main = () => {
+    getData();  
+    createObserver();  
+};
+
+document.addEventListener("DOMContentLoaded", main);
